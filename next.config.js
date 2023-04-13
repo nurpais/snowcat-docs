0f033c828a298efffec7e61cc06c476b263b06dc
Nurpais Orozulbaev <orozulbaev@gmail.com>
2023-04-13 20:58:01 +0600

    Add export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMarkdoc = require('@markdoc/next.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
     scrollRestoration: true,
